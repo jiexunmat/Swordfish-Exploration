919998017c24a15af0ce22873a0d8428410b90bc
Yao Yang Leow <yaoyang.leow@mclaren.com>
2017-08-24 16:15:35 +0100

    Added useful scripts

--- SimulationEngine.m
+++ SimulationEngine.m
@@ -1,22 +1,24 @@
-function [ responses ] = SimulationEngine( params,designVars )
+function [ responses ] = SimulationEngine( designVars )
 %SIMULATIONENGINE 
 % params = [l,X,Y,E]    % length, load_x, load_y, Young's modulus
 % designVars = [w,t]    % width, thickness
-% responses = [A,R,D]   % Area, Stress, Displacement
+% responses = [S,R,D]   % Area, Stress, Displacement
 
-% Unpack params and designVars
-l = params(1);
-X = params(2);
-Y = params(3);
-E = params(4);
+% Simulation parameters
+l = 100;
+X = 500;
+Y = 1000;
+E = 2.9E7;
 
+% Unpack designVars
 w = designVars(1);
 t = designVars(2);
 
+% Calculate responses
 A = w*t;
-R = 600*Y/(w*t^2) + 600*X/(w^2*t);
+S = 600*Y/(w*t^2) + 600*X/(w^2*t);
 D = 4*l^3 / (E*w*t) * sqrt((Y/t^2)^2 + (X/w^2)^2);
 
-responses = [A,R,D];
+responses = [A,S,D];
 end
 
